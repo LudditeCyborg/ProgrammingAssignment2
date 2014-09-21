6cbeb57f00f73233de4fd53bfe7e94b9769cae77
LudditeCyborg <jaredsupp@yahoo.com>
2014-09-21 15:50:25 -0500

    Added definitions for R functions in file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions can be used to cache matrix solutions
+## This allows matrix solutions to only be calculated once
 
-## Write a short comment describing this function
+## The function below takes a matrix and outputs a special object 
+## The special object is able to cache the matrix solution
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinvert <- function(invert) m <<- invert
+        getinvert <- function() m
+        list(set = set, get = get,
+             setinvert = setinvert,
+             getinvert = getinvert)
 }
 
 
-## Write a short comment describing this function
+## The below function takes the special object created by makeCacheMatrix
+## It attempts to retrieve the cached solution if it exists
+## Performs the solution if no cached solution already exists
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		m <- x$getinvert()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinvert(m)
+        m
 }
